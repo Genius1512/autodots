80c3d081b04b3b8e778593f6c452541acd39b50c
Silvan Schmidt <silvan.genius@gmail.com>
2022-08-28 16:32:03 +0200

    Make scripts executable in get

--- get_distro_creator.sh
+++ get_distro_creator.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/Genius1512/autodots/main/install.sh
+chmod +x install.sh
 
 mkdir .ad
 cd .ad
@@ -7,6 +8,7 @@ cd .ad
 wget https://raw.githubusercontent.com/Genius1512/autodots/main/.ad/copy_cfgs.sh
 wget https://raw.githubusercontent.com/Genius1512/autodots/main/.ad/install_fonts.sh
 wget https://raw.githubusercontent.com/Genius1512/autodots/main/.ad/install_pkgs.sh
+chmod +x .ad/*
 
 cd ..
 

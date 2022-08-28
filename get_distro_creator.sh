052f91e629719e40812c71e21d06aead030a97dd
Silvan Schmidt <silvan.genius@gmail.com>
2022-08-28 16:22:29 +0200

    Copy scripts to .dc

--- get_distro_creator.sh
+++ get_distro_creator.sh
@@ -2,8 +2,11 @@
 wget https://raw.githubusercontent.com/Genius1512/distro-creator/main/install.sh
 
 mkdir .dc
+cd .dc
 
 wget https://raw.githubusercontent.com/Genius1512/distro-creator/main/.dc/copy_cfgs.sh
 wget https://raw.githubusercontent.com/Genius1512/distro-creator/main/.dc/install_fonts.sh
 wget https://raw.githubusercontent.com/Genius1512/distro-creator/main/.dc/install_pkgs.sh
+
+cd ..
 
